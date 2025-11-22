c965fc94be8d3fe4983d0d2f258003b4caa42b16
oscilococcinum <bargaw28@gmail.com>
2025-11-22 22:39:51 +0100

    fixes old nixpkgs version

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     appimage-file-beta = {
       url = "https://github.com/zen-browser/desktop/releases/download/1.17.7b/zen-x86_64.AppImage";
       flake = false;
