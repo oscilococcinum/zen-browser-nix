442939a35a2e48ef9ec536a10c685fdb7e4c0515
oscilococcinum <gawlikbar@gmail.com>
2026-06-04 15:23:28 +0200

    updates zen to 1.20.2b

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    zen-appimage = {
-      url = "https://github.com/zen-browser/desktop/releases/download/1.19.8b/zen-x86_64.AppImage";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    appimage-file-beta = {
+      url = "https://github.com/zen-browser/desktop/releases/download/1.20.2b/zen-x86_64.AppImage";
       flake = false;
     };
   };
@@ -19,10 +19,10 @@
               config.allowUnfree = true;
             }; rec {
 
-              zen-appimage = pkgs.callPackage (import ./pkg/appimage-default.nix) {
-                src = inputs.zen-appimage;
+              zen-beta-appimage = pkgs.callPackage (import ./pkg/appimage-default.nix) {
+                src = inputs.appimage-file-beta;
                 pname = "zen";
-                version = "1.19.8b";
+                version = "1.20.2b";
               };
 
               default = zen-appimage;
